2ff73b75a331a28f67ace5aa7d7c8b8b42c1094b
Miguel Carvajal <krvajal.miguelangel@gmail.com>
2019-07-01 16:24:53 +0200

    feature: simplify JSON request (can be removed already)

--- src/JsonRequest.ts
+++ src/JsonRequest.ts
@@ -1,42 +1,15 @@
 import AbstractRequest from './AbstractRequest';
-import { parseJson } from './Middlewares';
 
 export default class JsonRequest extends AbstractRequest {
-    constructor(...args) {
-        super(...args);
+    constructor() {
+        super();
 
-        this.middlewares = [parseJson];
         this.withOptions({
             type: 'json',
             headers: {
                 Accept: 'application/json',
                 'Content-Type': 'application/json',
             },
-        });
-    }
-
-    //////////////////////////////////////////////////////////////////////
-    ////////////////////////////// REQUESTS //////////////////////////////
-    //////////////////////////////////////////////////////////////////////
-
-    put(url, payload) {
-        return this.make(url, {
-            method: 'PUT',
-            body: JSON.stringify(payload),
-        });
-    }
-
-    patch(url, payload) {
-        return this.make(url, {
-            method: 'PATCH',
-            body: JSON.stringify(payload),
-        });
-    }
-
-    post(url, payload) {
-        return this.make(url, {
-            method: 'POST',
-            body: JSON.stringify(payload),
         });
     }
 }
