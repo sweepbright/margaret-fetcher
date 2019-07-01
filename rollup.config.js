d3788777f890cd7a53deef9e52a1788052c4e7a6
Miguel Carvajal <krvajal.miguelangel@gmail.com>
2019-07-01 13:36:02 +0200

    chore(build): add support for TS

--- rollup.config.js
+++ rollup.config.js
@@ -7,7 +7,7 @@ import { terser } from 'rollup-plugin-terser';
 const isProd = process.env.NODE_ENV === 'production';
 
 export default {
-    input: 'src/index.js',
+    input: 'src/index.ts',
     output: {
         file: 'dist/index.js',
         format: 'umd',
@@ -16,11 +16,13 @@ export default {
     plugins: [
         babel({
             exclude: 'node_modules/**',
+            extensions: ['.js', '.jsx', '.ts', '.tsx'],
         }),
         nodeResolve({
             jsnext: true,
             main: true,
             preferBuiltins: true,
+            extensions: ['.js', '.jsx', '.ts', '.tsx'],
         }),
         commonjs({
             // non-CommonJS modules will be ignored, but you can also
